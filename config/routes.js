8e6fdbe587787d20170211b1458373f55c335ced
Luan Carlos Araldi <luan.araldi@gmail.com>
2017-04-24 19:34:37 -0300

    REturn requisition

--- config/routes.js
+++ config/routes.js
@@ -27,8 +27,7 @@ module.exports = function(app, passport) {
     }), function (req, res) {
         reactCookie.plugToRequest(req, res);
         res.cookie('usuario', req.user, {domain: "sigmaprojectclient.herokuapp.com"})
-        var retorno = req;
-        res.send(JSON.stringify(retorno));
+        res.send(req.body);
         //res.redirect("https://sigmaprojectclient.herokuapp.com/#/Dashboard")
     });
 
